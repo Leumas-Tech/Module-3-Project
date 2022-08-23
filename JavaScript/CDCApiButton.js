278384fe984e5a538192a82634a7cea35c9e5b77
william7829 <92896220+william7829@users.noreply.github.com>
2022-08-22 22:00:02 -0500

    problem again! will fix soon, need break

---
+++ JavaScript/CDCApiButton.js
@@ -0,0 +1,13 @@
+const CDCAPI = document.createElement('CDCAPI');
+
+CDCAPI.innerHTML = `
+<div class="floater">
+    <a href="#" class="float">
+    <i class="fa fa-plus my-float"></i>
+    </a>
+</div>`;
+
+    document.body.appendChild(CDCAPI);
+
+
+
