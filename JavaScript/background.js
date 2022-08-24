0993e93d1fc35f387a07d40ea43279a78a99178a
william7829 <92896220+william7829@users.noreply.github.com>
2022-08-23 20:55:31 -0500

    save before checkout

--- JavaScript/background.js
+++ JavaScript/background.js
@@ -3,6 +3,7 @@ const floatingTileBackground = document.createElement('floatingTileBackground');
 floatingTileBackground.innerHTML = ` <!-- Every page so we can have the animated background, we add a class area to a main and class circles to a div just like follows. And dont forget a bunch of LIs at the bottom. The link to the CSS that i Got this from is here. https://alvarotrigo.com/blog/animated-backgrounds-css/ -->
 <!-- I am also using Bootstrap 🙂 -->
 <main class="area">
+
   <div class="circles">
 
     <!-- This is for the animated background -->
