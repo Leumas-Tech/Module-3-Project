2daa1cdadefe666388a3efcb23dbb059b1565c61
william7829 <92896220+william7829@users.noreply.github.com>
2022-08-25 21:33:55 -0500

    Add files via upload

---
+++ JavaScript/searchDataAPI.js
@@ -0,0 +1,11 @@
+
+
+
+function SearchCovidData() {
+    document.getElementById("CovidInfo").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeCovidData() {
+    document.getElementById("CovidInfo").style.width = "0%";
+  }
---
+++ JavaScript/searchHospitalsApiButton.js
@@ -0,0 +1,11 @@
+
+
+
+function SearchHospitalData() {
+    document.getElementById("HospitalInfo").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeData() {
+    document.getElementById("HospitalInfo").style.width = "0%";
+  }
