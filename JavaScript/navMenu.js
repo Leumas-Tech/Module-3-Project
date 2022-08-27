a05173f2a6d11d1dbb5cbae180e7a9024deb9a5b
Flintryan82 <66896966+Flintryan82@users.noreply.github.com>
2022-08-27 10:29:16 -0600

    updated file paths between folders, noticed last push to github was not alphabetical order causing bad layout

--- JavaScript/navMenu.js
+++ JavaScript/navMenu.js
@@ -3,31 +3,31 @@ const NavigationMenu = document.createElement('NavigationMenu');
 NavigationMenu.innerHTML = `<div id="N" class="navMenu">
         <ul class="navigation">
          <li class="active">
-             <a href="/index.html">
+             <a href="../index.html">
                  <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
                  <span class="text">Home</span>
              </a>
          </li>
          <li>
-             <a href="/html/SymptomChecker.html">
+             <a href="../html/SymptomChecker.html">
                  <span class="icon"><ion-icon name="medkit-outline"></ion-icon></span>
                  <span class="text">Symptoms</span>
              </a>
          </li>
          <li>
-             <a href="/html/searchNearestHospital.html">
+             <a href="../html/searchNearestHospital.html">
                  <span class="icon"><ion-icon name="search-outline"></ion-icon></ion-icon></span>
                  <span class="text">Search Facilities</span>
              </a>
          </li>
          <li>
-             <a href="/html/covidDataSearchPage.html">
+             <a href="../html/covidDataSearchPage.html">
                  <span class="icon"><ion-icon name="star-outline"></ion-icon></span>
                  <span class="text">Trending</span>
              </a>
          </li>
          <li>
-             <a href="/html/AboutCovidAPI.html">
+             <a href="../html/AboutCovidAPI.html">
                  <span class="icon"><ion-icon name="medkit-outline"></ion-icon></span>
                  <span class="text">Symptom Checker</span>
              </a>
