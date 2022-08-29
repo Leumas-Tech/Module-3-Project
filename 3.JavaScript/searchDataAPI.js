1f30fccfe3307e444609b3b6a69f8f49f4dd7fc6
Flintryan82 <66896966+Flintryan82@users.noreply.github.com>
2022-08-28 20:57:11 -0600

    added updated files for API display

--- 3.JavaScript/searchDataAPI.js
+++ 3.JavaScript/searchDataAPI.js
@@ -1,7 +1,7 @@
 function SearchCovidData() {
 
   // Accessing input element
-  
+
   let input = document.getElementById('covidSearch').value; // This is the user input.
   document.getElementById("CovidInfo").style.width = "100%";
 
@@ -28,40 +28,43 @@ function SearchCovidData() {
   fetch(url, options)
     .then(response => response.json())
     .then(response => {
-      //Gather data received from API, Get access to Table.
+      //Gather data received from API, Get access to div.
       let yourResult = document.getElementById('covid-Info');
       let keys = Object.keys(response);
       let values = Object.values(response);
-      //Create a Table Row inside Table for Incoming Data
-      let trHeader = document.createElement('tr');
-      trHeader.setAttribute('id', 'Header');
-      //Loop Through Keys in API Object
+      // create an empty array to push created elements with Key information.
+      let divArr = [];
       for (let key of keys) {
-        //Create a Table Header for each Key
-        let th = document.createElement('th');
+        // Create a div to display the Key
+        let displayDiv = document.createElement("div");
+        displayDiv.setAttribute("id", "covidDisplayDiv");
+        // The key will be displayed in an H2 element.
+        let heading = document.createElement("h2");
         //The regex removed the unwanted characters behind the Key
-        let newKey = key.replace(/_\w+$/g, '');
-        th.textContent = `${newKey}`;
-        //Appended Table Header and Row
-        trHeader.appendChild(th);
-        yourResult.appendChild(trHeader);
+        let newKey = key.replace(/_\w+$/g, "");
+        // Set the innerHTML as the Key Data
+        heading.innerText = `${newKey}`;
+        //Appended Button
+        displayDiv.append(heading);
+        yourResult.appendChild(displayDiv);
+        divArr.unshift(displayDiv);
+        //
       }
-      //Create a Table Row inside Table for Incoming Data 
-      let trData = document.createElement('tr');
-      trData.setAttribute('id', 'Data');
-      //Loop through Values in API Object
+      // Loop through Values in API Object
       for (let value of values) {
-        let td = document.createElement('td');
         //Check If value is less than 0
+        let myValue = divArr.pop();
+        let myInfo = document.createElement("p");
+        myInfo.setAttribute("class", "myInfo");
         if (value <= 0) {
-          value = 'N/A';
-          td.textContent = `${value}`; // Set values to Table Data Element
-          trData.appendChild(td);
-          yourResult.appendChild(trData);
+          value = "N/A";
+          myInfo.innerHTML = `${value}`; // Set value to paragraph element.
+          myValue.append(myInfo);
+          yourResult.appendChild(myValue);
         } else {
-          td.textContent = `${value}`; // Set values to Table Data Element
-          trData.appendChild(td);
-          yourResult.appendChild(trData);
+          myInfo.innerHTML = `${value}`; // Set values to paragraph Element.
+          myValue.append(myInfo);
+          yourResult.appendChild(myValue);
         }
       }
       console.log(response);
@@ -76,4 +79,11 @@ function SearchCovidData() {
 /* Close when someone clicks on the "x" symbol inside the overlay */
 function closeCovidData() {
   document.getElementById("CovidInfo").style.width = "0%";
+  // Accessing the elements created to delete on window close.
+  const myInfo = document.getElementById('covid-Info');
+  myInfo.textContent = "";
+  const mySearch = document.getElementById('covidSearch');
+  mySearch.value = "";
+  // Remove elements
+
 }
--- 3.JavaScript/searchHospitalsApiButton.js
+++ 3.JavaScript/searchHospitalsApiButton.js
@@ -1,11 +1,65 @@
-
-
-
 function SearchHospitalData() {
-    document.getElementById("HospitalInfo").style.width = "100%";
+
+  // Accessing Input element.
+
+  let input = document.getElementById('hospitalSearch').value; // This is user input...
+  document.getElementById("HospitalInfo").style.width = "100%";
+
+  // Url for Hospital API
+  let url = 'https://us-hospitals.p.rapidapi.com/?name=presbyterian';
+  // Regex to remove 
+  let regex = /\=[a-zA-Z]+/g;
+  // if else statement to check for empty input.
+  if (input.length == 0) {
+    // alert("Please Enter a Valid Hospital Name.");
+  } else {
+    url = url.replace(regex, '=' + input);
   }
-  
-  /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeData() {
-    document.getElementById("HospitalInfo").style.width = "0%";
-  }
+  console.log(url);
+  // API Fetch Code................................
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'fdc2bfa2c7msh9bbefe74000b7fbp134f42jsn20bc45c4a096',
+      'X-RapidAPI-Host': 'us-hospitals.p.rapidapi.com'
+    }
+  };
+
+  fetch(url, options)
+    .then(response => response.json())
+    .then(response => {
+
+      //Gather data from API results, get access to display.
+      const myResult = document.getElementById('display');
+
+      let i = 0;
+      // set up while loop to go through array of objects.
+      while (i <= response.length - 1) {
+        let keys = Object.keys(response[i]);
+        let value = Object.values(response[i]);
+        // console.log(`${keys}:${value}`);
+        const section = document.createElement('div');
+        const header = document.createElement('h2');
+        const text = document.createElement('p');
+        header.textContent = keys[i];
+        text.textContent = value[i];
+        section.append(header,text);
+        myResult.appendChild(section);
+
+        i++;
+      }
+
+    });
+
+  //   .catch(error => {
+  //     let message = document.getElementById('covidInfo');
+  //     message.innerHTML = `Error: ${error}`;
+  //     // message.append(message.innerHTML);
+  // });
+
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeData() {
+  document.getElementById("HospitalInfo").style.width = "0%";
+}
